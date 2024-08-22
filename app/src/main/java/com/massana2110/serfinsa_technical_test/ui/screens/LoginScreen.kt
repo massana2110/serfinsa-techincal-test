f711d3744040a2464984f60bf3569ed6695e75e9
David Massana <david.massana.10@gmail.com>
2024-08-22 14:53:03 -0600

    :tada: First commit

---
+++ app/src/main/java/com/massana2110/serfinsa_technical_test/ui/screens/LoginScreen.kt
@@ -0,0 +1,16 @@
+package com.massana2110.serfinsa_technical_test.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun LoginScreenPreview() {
+    LoginScreen()
+}
